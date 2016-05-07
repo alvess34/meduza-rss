bcb95a5b82b26ff332e288a666678280e5705177
alvess34 <solt1234@yandex.ru>
2016-05-07 21:08:26 +0400

    Update meduza-rss.rb

--- meduza-rss.rb
+++ meduza-rss.rb
@@ -2,19 +2,19 @@ require 'open-uri'
 require 'json'
 require 'nokogiri'
 
-$meduza = 'https://meduza.io'
+$meduza = 'gamemag.ru'
 $meduza_rss = $meduza + '/rss/%s'
 $meduza_api = $meduza + '/api/v3/%s'
 
 $feeds = %w(all news fun)
 
-class Meduza
-  def Meduza.start
+class Gamemaf=g
+  def Gamemag.start
     @cached = {}
     Thread.new do
       while true do
         $feeds.each do |feed|
-          @cached[feed] = Meduza.generate(feed)
+          @cached[feed] = Gamemag.generate(feed)
           sleep 10
         end
         sleep 30*60
@@ -22,10 +22,10 @@ class Meduza
     end
   end
 
-  def Meduza.rss(feed = 'all')
+  def Gamemag.rss(feed = 'all')
     if @cached[feed].nil?
       puts 'generating new feed'
-      Meduza.generate(feed)
+      Gamemag.generate(feed)
     else
       puts 'getting a feed from the cache'
       @cached[feed]
@@ -34,14 +34,14 @@ class Meduza
 
   private
 
-  def Meduza.generate(feed)
-    doc = Nokogiri::XML(open($meduza_rss % feed))
+  def Gamemag.generate(feed)
+    doc = Nokogiri::XML(open($gamemag_rss % feed))
 
     doc.xpath('/rss/channel/item').each do |item|
-      post_id = item.xpath('link').inner_text.gsub(/^#{$meduza}\//, '')
+      post_id = item.xpath('link').inner_text.gsub(/^#{$gamemag}\//, '')
       json = JSON::parse(open($meduza_api % post_id).read)
       item.search('description').each do |description|
-        description.content = json['root']['content']['body'].gsub('src="/image/', 'src="//meduza.io/image/')
+        description.content = json['root']['content']['body'].gsub('src="/image/', 'src="//gamemag.ru/image/')
       end
     end
 
@@ -49,11 +49,11 @@ class Meduza
   end
 end
 
-Meduza.start
+Gamemag.start
 
 __END__
 10.times do
-  Meduza.rss
-  Meduza.rss('news')
-  Meduza.rss('fun')
+  Gamemag.rss
+  Gamemag.rss('news')
+  Gamemag.rss('fun')
 end
